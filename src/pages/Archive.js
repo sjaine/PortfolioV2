4e5c2ddf608e24ea01676a47cf6fcfc72ad611cf
Jaine <jaineee00@gmail.com>
2025-01-26 12:30:08 -0500

    Archive Page

---
+++ src/pages/Archive.js
@@ -0,0 +1,53 @@
+import React from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import '../index.css';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+
+const projects = [
+    { id: 1, title: 'arbor day', thumbnail: "assets/img/arborday.gif" },
+    { id: 2, title: 'movie ticket', thumbnail: "/assets/img/movieticket.png" },
+    { id: 3, title: 'planting app', thumbnail: "/assets/img/watering.png" },
+    { id: 4, title: 'book redesign', thumbnail: "/assets/img/book.png" },
+    { id: 5, title: 'movie night', thumbnail: "/assets/img/movienight.png" },
+    { id: 6, title: 'movie poster', thumbnail: "/assets/img/movieposter.png" }
+  ];
+
+function Archive() {
+  return (
+    <div className="archive">
+        {/* header */}
+        <div className="link main color_orange">
+            <div className="gotoHome">👋 sjaine.me/work</div>
+            <div className="copyright main color_orange">© 2025 Yoonsoo Jaine Shin <br />
+Hand-coded with ❤️ using React</div>
+        </div>
+        {/* sections */}
+        <Swiper
+            slidesPerView={3.5}
+            spaceBetween={50}
+            grabCursor={true}
+            modules={[Pagination]}
+            initialSlide={0}
+            className="mySwiper"
+            style={{ padding: "45px" }}
+        >
+            {projects.map((project) => (
+                <SwiperSlide className="archive_container">
+                    <div className="archive_image">
+                        <img src={project.thumbnail} alt={`${project.title} thumbnail`} />
+                    </div>
+                </SwiperSlide>
+            ))}
+        </Swiper>
+    </div>
+  );
+}
+
+export default Archive;
